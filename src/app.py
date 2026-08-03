97c2b3610748968aa9f0a8fca5da54f88275fb75
Maria Veronica Suarez Vidal <veronicasuarezvidal@gmail.com>
2026-08-03 19:28:21 -0400

    proyecto

--- src/app.py
+++ src/app.py
@@ -1,4 +1,32 @@
-from utils import db_connect
-engine = db_connect()
+import xgboost as xgb
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+model = xgb.XGBClassifier()
+model.load_model('../models/xgb_model.json')
+class_dict = {
+    "0": "No tiene diabetes",
+    "1": "Tiene diabetes"
+}
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        val4 = float(request.form["val4"])
+        val5 = float(request.form["val5"])
+        val6 = float(request.form["val6"])
+        val7 = float(request.form["val7"])
+        val8 = float(request.form["val8"])
+        
+        data = [[val1, val2, val3, val4, val5, val6, val7, val8]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction = pred_class)
